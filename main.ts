5088ef886869bf9f5c9155fab7f838cfac116445
dsimfukwe <dumisimfukwe@yahoo.com>
2022-08-25 17:48:01 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,20 @@
 basic.forever(function () {
-    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
-        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 60)
-    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
-        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 160)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 30)
-    } else {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 60) {
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 30)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 160)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 180)
+    } else {
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 60)
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+        music.playMelody("C - C - C - C5 - ", 120)
     }
 })
