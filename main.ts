9b7b496d1e05dd3ea00ff88ec6a1c46c869a47b6
dsimfukwe <dumisimfukwe@yahoo.com>
2022-08-25 16:14:35 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,13 +2,15 @@ basic.forever(function () {
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 70)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 60)
     } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 160)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 30)
     } else {
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 30)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 160)
     }
